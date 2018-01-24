dc309568f538956b8b693c3f90edead0827c0842
John Luke Saake <johnlukesaake@gmail.com>
2018-01-24 13:50:29 -0800

    Continued work on Gamemanger
    Added variables and some sub-state machine basework for gameobject.
    Consider saving prefabs in a list of gameobjects for each type of ball for the currentBall to clone

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,13 +4,81 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Player player;                       // Player sprite
+    public Ball currentBall;                    // To be used for current ball in play
+    public float power;                         // Power of the shot
+    public float angle;                         // Angle of the shot
+    public float maxShots = 8;                  // Max number of shots: Should equal length of ShotTypes
+    public float numShots = 0;                  // Number of shots taken already
+    public enum ShotTypes { BASEBALL, FRISBEE, NERFFOOTBALL, FOOTBALL,
+        SOCCERBALL, VOLLEYBALL, TENNISBALL, HOCKEYPUCK };
+    public ShotTypes selectedShot;              // Actively selected shot type
+
+    // Has type of shot been used
+    public bool usedBaseball = false;
+    public bool usedFrisbee = false;
+    public bool usedNerfFootball = false;
+    public bool usedFootball = false;
+    public bool usedSoccerBall = false;
+    public bool usedVolleyBall = false;
+    public bool usedTennisBall = false;
+    public bool usedHockeyPuck = false;
+
+
+
+    // Use this for initialization
+    void Start() {
+        player.MovePlayer(player.PLAYER_START_X, player.PLAYER_START_Y);
+        ResetUsage();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+
+    void Shot (float power, float angle)
+    {
+
+    }
+
+    float _DetermineShotStartHeight (ShotTypes)
+    {
+        if (selectedShot == ShotTypes.BASEBALL ||
+            selectedShot == ShotTypes.FOOTBALL ||
+            selectedShot == ShotTypes.FRISBEE ||
+            selectedShot == ShotTypes.NERFFOOTBALL ||
+            selectedShot == ShotTypes.TENNISBALL ||
+            selectedShot == ShotTypes.VOLLEYBALL)
+        {
+            return player.BALL_UPPER_Y;
+        } else
+        {
+            return player.BALL_LOWER_Y;
+        }
+    }
+
+    void EndStroke ()
+    {
+        player.MovePlayer(currentBall.transform.position.x, player.PLAYER_START_Y);
+        Destroy(currentBall.gameObject);
+    }
+
+    void ResetUsage ()
+    {
+        usedBaseball = false;
+        usedFrisbee = false;
+        usedNerfFootball = false;
+        usedFootball = false;
+        usedSoccerBall = false;
+        usedVolleyBall = false;
+        usedTennisBall = false;
+        usedHockeyPuck = false;
+}
+
+    void SelectShotType (ShotTypes shotType)
+    {
+        //TODO: state machine for checking if each shot type can be selected
+    }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float PLAYER_START_X = 0;
+    public float PLAYER_START_Y = 4;
+    public float BALL_LOWER_Y = 1;
+    public float BALL_UPPER_Y = 5;
+
+   public void MovePlayer (float xPosition, float yPosition)
+    {
+        this.gameObject.transform.position = new Vector3(xPosition, yPosition, 0);
+    }
+}
