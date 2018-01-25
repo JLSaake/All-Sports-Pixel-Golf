8ea50d48be3c762ac4cd50acae590359da26f095
John Luke Saake <johnlukesaake@gmail.com>
2018-01-24 20:27:10 -0800

    Some attemtps to get Flag & Ball to trigger
    Backup save, but trying to get flag and ball to trigger each other, which isn't happening at the moment

--- Assets/Scripts/Flag.cs
+++ Assets/Scripts/Flag.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Flag : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void OnTriggerEnter2D (Collider2D collider)
+    {
+        Debug.Log("Something triggered me");
+        if (collider.gameObject.tag == "Ball")
+        {
+            Debug.Log("It's in the hole!");
+            // Call game manager ending
+            // Throw fireworks at flag
+        }
+    }
+    
 }
